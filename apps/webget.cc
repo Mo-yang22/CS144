df2f733f7bee6ad287300d859cf8f3539ea60644
erdog <sss@qq.com>
2023-03-21 12:37:19 +0000

    lab 1 finish

--- apps/webget.cc
+++ apps/webget.cc
@@ -7,18 +7,17 @@
 using namespace std;
 
 void get_URL(const string &host, const string &path) {
-    // Your code here.
-
-    // You will need to connect to the "http" service on
-    // the computer whose name is in the "host" string,
-    // then request the URL path given in the "path" string.
-
-    // Then you'll need to print out everything the server sends back,
-    // (not just one call to read() -- everything) until you reach
-    // the "eof" (end of file).
-
-    cerr << "Function called: get_URL(" << host << ", " << path << ").\n";
-    cerr << "Warning: get_URL() has not been implemented yet.\n";
+    TCPSocket sock1;
+    Address addr = Address(host, "http");
+    sock1.connect(addr);
+    sock1.write("GET " + path + " " + "HTTP/1.1\r\nHost: " + host + "\r\nConnection: close\r\n\r\n");
+    while (1) {
+        auto recv = sock1.read();
+        cout << recv;
+        if (sock1.eof())
+            break;
+    }
+    sock1.close();
 }
 
 int main(int argc, char *argv[]) {
--- libsponge/stream_reassembler.cc
+++ libsponge/stream_reassembler.cc
@@ -12,15 +12,47 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 
 using namespace std;
 
-StreamReassembler::StreamReassembler(const size_t capacity) : _output(capacity), _capacity(capacity) {}
+StreamReassembler::StreamReassembler(const size_t capacity) : _output(capacity), _capacity(capacity),_buf(capacity,'\0'),_flag(capacity,false) {}
 
 //! \details This function accepts a substring (aka a segment) of bytes,
 //! possibly out-of-order, from the logical stream, and assembles any newly
 //! contiguous substrings and writes them into the output stream in order.
 void StreamReassembler::push_substring(const string &data, const size_t index, const bool eof) {
-    DUMMY_CODE(data, index, eof);
+    size_t first_unassembled=_output.bytes_written();
+    size_t first_unacceptable=first_unassembled+_capacity;
+    if(index>=first_unacceptable || index+data.size()<first_unassembled)
+        return;
+    size_t begin_index=index;
+    size_t end_index=index+data.size();
+    if(begin_index<first_unassembled)
+        begin_index=first_unassembled;
+    if(end_index>first_unacceptable)
+        end_index=first_unacceptable;
+    for(size_t i=begin_index;i<end_index;++i){
+        if(!_flag[(i)%_capacity]){
+            _buf[(i)%_capacity]=data[i-index];
+            _flag[(i)%_capacity]=true;
+            unass_bytes++;
+        }
+    }
+    string tmp;
+    while(_flag[_index%_capacity]){
+        tmp+=_buf[_index%_capacity];
+        _flag[_index%_capacity]=false;
+        _index++;
+        unass_bytes--;
+    }
+    if(tmp.size()>0)
+        _output.write(tmp);
+    if(eof)
+    {
+        _eof=eof;
+        _eof_index=end_index;
+    }
+    if(_eof&&_eof_index==_output.bytes_written())
+        _output.end_input();
 }
 
-size_t StreamReassembler::unassembled_bytes() const { return {}; }
+size_t StreamReassembler::unassembled_bytes() const { return unass_bytes; }
 
-bool StreamReassembler::empty() const { return {}; }
+bool StreamReassembler::empty() const { return (unassembled_bytes()==0); }
